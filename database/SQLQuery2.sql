80b11566a5ab32dff044a0d38efe9e41cfca7b99
sofia22006 <irmamoreno1129873@correo.itm.edu.co>
2026-04-07 19:25:47 -0500

    Add insert statements for multiple tables
    Added insert statements for TipoVehiculo, Vehiculo, EspacioParqueadero, Mensualidad, and Ticket tables.

--- database/SQLQuery2.sql
+++ database/SQLQuery2.sql
@@ -45,3 +45,29 @@ Create table Ticket (
 	foreign key (placa) references Vehiculo(placa),
 	foreign key (id_espacio) references EspacioParqueadero(id_espacio)
 );
+
+---- insert
+Insert TipoVehiculo values
+(1, 'auto', 50),
+(2, 'motocicleta', 30),
+(1, 'auto', 50);
+
+insert Vehiculo values
+('ABC123', 1, 'corolla', 'Blanco'),
+('DEF45G', 2, 'YZF', 'negro'),
+('HIJ678', 1, 'spark', 'gris');
+
+insert EspacioParqueadero values
+(1, 101, 1, 'auto', 1),
+(2, 105, 2, 'motocicleta', 1),
+(3, 103, 3, 'auto', 1);
+
+insert Mensualidad values
+(01, 'ABC123', '2026-04-07', '2026-04-08', 1),
+(02, 'DEF45G', '2024-09-11', '2024-09-11', 1),
+(03, 'HIJ678', '2026-08-02', '2026-08-03', 0);
+
+insert Ticket values 
+(1, 'ABC123', 1, '08:30:00', '10:12:00', 78000),
+(5, 'DEF45G', 2, '12:20:00', '16:22:09',7200 ),
+(6, 'HIJ678', 3, '20:15:00', '08:50:00', 39000);
