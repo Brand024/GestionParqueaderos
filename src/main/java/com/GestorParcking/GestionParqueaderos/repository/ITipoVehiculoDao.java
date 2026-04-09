1d7584b7715e47a2a0dbc8b9e09621d29969cce7
sofia22006 <irmamoreno1129873@correo.itm.edu.co>
2026-04-08 21:41:22 -0500

    Update ITipoVehiculoDao.java

--- src/main/java/com/GestorParcking/GestionParqueaderos/repository/ITipoVehiculoDao.java
+++ src/main/java/com/GestorParcking/GestionParqueaderos/repository/ITipoVehiculoDao.java
@@ -3,6 +3,7 @@ package com.GestorParcking.GestionParqueaderos.repository;
 import com.GestorParcking.GestionParqueaderos.models.TipoVehiculo;
 import java.util.List;
 
+//define las operaciones básicas sobre la entidad TipoVehiculo
 public interface ITipoVehiculoDao {
     List<TipoVehiculo> listarTipos();
     TipoVehiculo buscarPorId(int idTipo);
