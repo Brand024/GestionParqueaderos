5c2587cab03959afbe633a1463e7a40b9c9e986a
sofia22006 <irmamoreno1129873@correo.itm.edu.co>
2026-04-08 21:41:52 -0500

    Add comment to ITicketDao interface
    Added a comment to describe the ITicketDao interface.

--- src/main/java/com/GestorParcking/GestionParqueaderos/repository/ITicketDao.java
+++ src/main/java/com/GestorParcking/GestionParqueaderos/repository/ITicketDao.java
@@ -3,6 +3,7 @@ package com.GestorParcking.GestionParqueaderos.repository;
 import com.GestorParcking.GestionParqueaderos.models.Ticket;
 import java.util.List;
 
+//define las operaciones básicas sobre la entidad Ticket
 public interface ITicketDao {
     void generarTicketEntrada(Ticket ticket);
     void registrarSalida(Ticket ticket);
