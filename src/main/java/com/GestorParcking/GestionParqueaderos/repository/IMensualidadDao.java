16dde40ce4b038c24e001e9362216fd3359c2e83
sofia22006 <irmamoreno1129873@correo.itm.edu.co>
2026-04-08 21:42:24 -0500

    Add comment to IMensualidadDao interface
    Added a comment to describe the basic operations for the Mensualidad entity.

--- src/main/java/com/GestorParcking/GestionParqueaderos/repository/IMensualidadDao.java
+++ src/main/java/com/GestorParcking/GestionParqueaderos/repository/IMensualidadDao.java
@@ -3,6 +3,7 @@ package com.GestorParcking.GestionParqueaderos.repository;
 import com.GestorParcking.GestionParqueaderos.models.Mensualidad;
 import java.util.List;
 
+//define las operaciones básicas sobre la entidad Mensualidad
 public interface IMensualidadDao {
     void registrar(Mensualidad mensualidad);
     List<Mensualidad> listarTodas();
