496a82175d0f586ab9c0b354da97282ee12b18da
sofia22006 <irmamoreno1129873@correo.itm.edu.co>
2026-04-08 21:40:43 -0500

    Add comment to IVehiculoDao interface
    Added a comment to define basic operations for the Vehiculo entity.

--- src/main/java/com/GestorParcking/GestionParqueaderos/repository/IVehiculoDao.java
+++ src/main/java/com/GestorParcking/GestionParqueaderos/repository/IVehiculoDao.java
@@ -3,6 +3,7 @@ package com.GestorParcking.GestionParqueaderos.repository;
 import com.GestorParcking.GestionParqueaderos.models.Vehiculo;
 import java.util.List;
 
+//define las operaciones básicas sobre la entidad Vehiculo
 public interface IVehiculoDao {
     void registrar(Vehiculo vehiculo);
     Vehiculo buscarPorPlaca(String placa);
